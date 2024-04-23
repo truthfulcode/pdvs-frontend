2791fe1d12a80d3d12b920bd34acccc34d696e70
invalidpushpop <imazziamine@graduate.utm.my>
2024-04-23 12:31:58 +0800

    fix: tailwind bug

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,7 +1,7 @@
 import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
 
 import { cookieStorage, createStorage } from "wagmi";
-import { mainnet, polygonMumbai, sepolia } from "wagmi/chains";
+import { mainnet, sepolia } from "wagmi/chains";
 
 // Get projectId at https://cloud.walletconnect.com
 export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
