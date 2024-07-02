1be4c7de1e5da2b05cbffd9396bb437de886c21d
truthfulcode <imazziamine@graduate.utm.my>
2024-07-02 08:20:59 +0800

    edit design of glossary

--- pages/glossary.tsx
+++ pages/glossary.tsx
@@ -1,46 +1,35 @@
 // "use client";
 import NavBar from "@/components/NavBar";
-import { Box, Typography } from "@mui/material";
+import { Box, List, ListItem, Typography } from "@mui/material";
 import { useAccount } from "wagmi";
+import styles from "../styles/page.module.css";
 
+const list = [{ t: "CM", c: "Committee Member" }, { t: "VP", c: "Voting Power" }, { t: "Block Snapshot", c: "Block number at which the proposal voting power would be retrieved from, thinking of it like freezing time in the past, using the voting power of that time." }]
 export default function Home() {
   return (
+
     <main>
       <NavBar />
-      <Box>
-        <Typography>Glossary</Typography>
-        <p>
-          <b>CM:</b> Committee Member
-        </p>
-        <p>
-          <b>VP:</b> Voting Power
-        </p>
-        <p>
-          <b>Block Snapshot:</b> Block number at which the proposal voting power
-          would be retrieved from, thinking of it like freezing time in the
-          past, using the voting power of that time.
-        </p>
-        <p>
-          <b>Proposal states:</b>
-          <ul style={{ paddingLeft: 8 }}>
-            <li>
-              <b>Draft</b>
-              The proposal is only accessible by the admin and Comittee Members,
-              they are able to edit the proposals.
-            </li>
-            <li>
-              <b>Active</b>A state, at which anyone can access the proposal, in
-              addition students are able to comment on the active proposals, as
-              well as liking the proposals.
-            </li>
-            <li>
-              <b>Finalized</b>A finalized state, at whihc no more editing can be
-              applied nor comemnts can be added, the proposal is pushed to
-              snapshot for voting, any students can vote within the proposal
-              voting period.
-            </li>
-          </ul>
-        </p>
+      <Box sx={{
+        p: 8,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        alignItems: "flex-start"
+      }}>
+        <Typography variant="h5" sx={{ fontWeight: "bold", margin: "auto" }}>Glossary</Typography>
+        <List>
+          {list.map(l => (
+            <ListItem sx={{ display: "flex" }}>
+              <Typography sx={{ flex: 1, fontWeight: "bold" }}>
+                {l.t}:
+              </Typography>
+              <Typography sx={{ flex: 5 }}>
+                {l.c}:
+              </Typography>
+            </ListItem>
+          ))}
+        </List>
       </Box>
     </main>
   );
