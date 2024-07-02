08281ed43aa746528c885253f0e870235180ada8
truthfulcode <imazziamine@graduate.utm.my>
2024-07-02 18:25:50 +0800

    black background instead of base

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -13,7 +13,7 @@ export default function NavBar() {
 
   React.useEffect(() => { console.log("error", error) }, [error])
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-black">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
